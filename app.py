c9762d433ffdaa3b8f965ec86373cda4a5660e03
lsejun <jun020619@naver.com>
2025-08-28 10:45:51 +0900

    0828

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import streamlit as st
 
 page_main = st.Page("app.py", title="Main Page", icon="👉")
-p1 = st.Page("user_input.py",title="Page 1",icon="🔧")
-p2 = st.Page("p1.py", title="Page 2",icon="🔧")
+p1 = st.Page("p1.py",title="Main Page",icon="🔧")
 
 
-page=st.navigation([p1,p2])
+page=st.navigation([p1])
 page.run()
--- main.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.markdown("잼민.이 mainpage page")
-st.sidebar.markdown("Clicked Main Page")
